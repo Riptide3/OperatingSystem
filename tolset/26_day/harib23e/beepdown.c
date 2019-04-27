df2b12781dacaa0d5d6338fd110587b87cbe3604
Riptide3 <riptide.yzy@gmail.com>
2019-04-27 17:18:06 +0800

    add day 25,26 and 27

---
+++ tolset/26_day/harib23e/beepdown.c
@@ -0,0 +1,25 @@
+void api_end(void);
+int api_getkey(int mode);
+int api_alloctimer(void);
+void api_inittimer(int timer, int data);
+void api_settimer(int timer, int time);
+void api_beep(int tone);
+
+void HariMain(void)
+{
+	int i, timer;
+	timer = api_alloctimer();
+	api_inittimer(timer, 128);
+	for (i = 20000000; i >= 20000; i -= i / 100) {
+		/* 20KHz~20Hz，即人类可以听到的声音范围 */
+		/* i以1%的速度递减 */
+		api_beep(i);
+int api_openwin(char *buf, int xsiz, int ysiz, int col_inv, char *title);
+		api_settimer(timer, 1);		/* 0.01秒 */
+		if (api_getkey(1) != 128) {
+			break;
+		}
+	}
+	api_beep(0);
+	api_end();
+}
